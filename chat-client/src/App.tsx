670601d2dfab561dba0050ed979dcb9dc19dc294
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2023-09-11 16:56:17 +0200

    deploy front app gh pages

--- chat-client/src/App.tsx
+++ chat-client/src/App.tsx
@@ -1,31 +1,17 @@
-import { io } from 'socket.io-client';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage';
 import { ChatPage } from './pages/ChatPage/ChatPage';
 import { NotFound } from './pages/NotFound/NotFound';
 import { RestrictedRoute } from './components/RestrictedRoute';
 import { ProtectedRoute } from './components/ProtectedRoute';
-import { useSelector } from 'react-redux';
-import { selectAuthUserIsLoggedIn } from './redux/user/userSelectors';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from './redux/store';
-import { setSocket } from './redux/globals/globalsSlice';
-import { selectAuthUserId } from './redux/user/userSelectors';
-import { setSocketReady } from './redux/globals/globalsSlice';
-import { selectAuthUserUsername } from './redux/user/userSelectors';
-import { initializeSocket, getSocket } from './services/socketService';
 import { refreshUser } from './redux/user/userOperations';
-import { selectSocketReady } from './redux/globals/globalsSelectors';
-import { useNavigate } from 'react-router-dom';
 
 export let socket: any;
 const App = () => {
   const dispatch: AppDispatch = useDispatch();
-  const socketReady = useSelector(selectSocketReady);
-  const isLoggedin = useSelector(selectAuthUserIsLoggedIn);
-  const userId = useSelector(selectAuthUserId);
-  const userName = useSelector(selectAuthUserUsername);
 
   useEffect(() => {
     const refreshUserData = async () => {
@@ -34,27 +20,6 @@ const App = () => {
 
     refreshUserData();
   }, []);
-
-  // useEffect(() => {
-  //   const initializeSocketAndRedux = async () => {
-  //     if (isLoggedin) {
-  //       const socket = initializeSocket({ userName, userId });
-  //       await new Promise<void>(resolve => {
-  //         socket.on('connect', () => {
-  //           resolve();
-  //         });
-  //       });
-
-  //       if (socket !== undefined) {
-  //         // dispatch(setSocket(socket));
-  //         dispatch(setSocketReady(true));
-  //       }
-  //     }
-  //   };
-
-  //   initializeSocketAndRedux();
-  //   // if (socketReady) navigate('/chat');
-  // }, [isLoggedin, socket]);
 
   return (
     <>
